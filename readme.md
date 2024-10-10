060b47c02dfcedf261f9c3e8eb1fc17462cb5afd
Yuliia Dovbysh <juli3stepura@gmail.com>
2024-10-10 22:40:14 +0300

    Solution

---
+++ readme.md
@@ -0,0 +1,44 @@
+# QA Restore names ([uk](readme.uk.md))
+
+The database failed. Some users lost values from the `firstName` field, but
+`user` object has a `fullName` field, so we can restore `firstName`.
+
+Write tests for `restoreNames` function, that takes an array of `users` and set
+correct `firstName` to users who do not have it or is equal to `undefined`. You
+should not return anything from the function.
+
+Example:
+
+```js
+const users = [
+  {
+    firstName: undefined,
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+  },
+  {
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  },
+];
+
+restoreNames(users);
+
+// users === [
+  {
+    firstName: 'Jack',
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+  },
+  {
+    firstName: 'Mike',
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  },
+];
+```
+
+---
+
+- [Guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md);
+- Read more about [Jest expectations](https://jestjs.io/uk/docs/expect).
---
+++ src/restoreNames.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+describe('restoreNames', () => {
+  const { restoreNames } = require('./restoreNames');
+
+  it(`'firstName' is 'undefined'`, () => {
+    const user = {
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    };
+
+    restoreNames([user]);
+
+    expect(user).toMatchObject({
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    });
+  });
+
+  it(`'firstName' was deleted`, () => {
+    const user = {
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    };
+
+    restoreNames([user]);
+
+    expect(user).toMatchObject({
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    });
+  });
+
+  it(`valid user`, () => {
+    const user = {
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    };
+
+    restoreNames([user]);
+
+    expect(user).toMatchObject({
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    });
+  });
+
+  it(`should return 'undefined'`, () => {
+    const user = {
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    };
+
+    expect(restoreNames([user])).toBeUndefined();
+  });
+});
+
+
